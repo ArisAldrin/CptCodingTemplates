030d262b2c410def293526501b7c59f99cd520c2
ArisAldrin <1469207005@qq.com>
2025-07-25 19:30:33 +0800

    2025.7.25,19:30

---
+++ CptCodeTemplate.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+#define rerr cerr << "\033[31m"
+#define erl "\033[0m" << '\n'
+#define NO cout << "NO" << '\n';
+#define YES cout << "YES" << '\n';
+#define No cout << "No" << '\n';
+#define Yes cout << "Yes" << '\n';
+#define NOO cout << -1 << '\n';
+#define fs first
+#define sc second
+#define int long long
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using db = double;
+using pii = pair<int , int>;
+using pll = pair<ll , ll>;
+using pdd = pair<db , db>;
+using i128 = __int128_t;
+
+const int inf = 1e9 + 7;
+const int nil = -inf;
+const ll INF = 1e18;
+const ll NIL = -INF;
+const ll MOD = 998244353;
+const db EPS = 1e-10;
+mt19937 rdint(time(0));
+mt19937_64 rdll(time(0));
+
+void solve(){
+    
+}
+
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;cin >> t;
+    while(t --)
+    solve();
+    return 0;
+}
