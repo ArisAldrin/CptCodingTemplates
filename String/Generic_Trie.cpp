b1d72916456d5b974d0747155e91f83edc08d999
ArisAldrin <1469207005@qq.com>
2025-10-28 20:42:40 +0800

    optmize style: ./String/Generic_Trie.cpp

--- String/Generic_Trie.cpp
+++ String/Generic_Trie.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 class TrieNode{
 public:
-    unordered_map<char,TrieNode*>child;
+    unordered_map<char , TrieNode*>child;
     // map<char,TrieNode*>child;
-    int cnt,cntpre;
+    int cnt , cntpre;
     bool isend;
-    TrieNode(){cnt=0; cntpre=0; isend=false;}
+    TrieNode(){cnt = 0; cntpre = 0; isend = false;}
 };
 
 class Generic_Trie{
@@ -16,34 +16,34 @@ private:
     TrieNode* rt;
 public:
     Generic_Trie(){
-        rt= new TrieNode();
+        rt = new TrieNode();
     }
     void insert(string s){
-        TrieNode* now=rt;
+        TrieNode* now = rt;
         for(auto i:s){
-            now->cntpre++;
-            if(now->child.find(i)==now->child.end()){
-                now->child[i]=new TrieNode();
+            now -> cntpre++;
+            if(now -> child.find(i) == now -> child.end()){
+                now -> child[i] = new TrieNode();
             }
-            now=now->child[i];
+            now = now -> child[i];
         }
-        now->cnt++; now->cntpre++; now->isend=true;
+        now -> cnt++; now -> cntpre++; now -> isend = true;
     }
     int askpre(string s){
-        TrieNode* now=rt;
+        TrieNode* now = rt;
         for(auto i:s){
-            if(now->child.find(i)==now->child.end())return 0;
-            now=now->child[i];
+            if(now -> child.find(i) == now -> child.end())return 0;
+            now = now -> child[i];
         }
-        return now->cntpre;
+        return now -> cntpre;
     }
     int find(string s){
-        TrieNode* now=rt;
+        TrieNode* now = rt;
         for(auto i:s){
-            if(now->child.find(i)==now->child.end())return false;
-            now=now->child[i];
+            if(now -> child.find(i) == now -> child.end())return false;
+            now = now -> child[i];
         }
-        return now->isend;
+        return now -> isend;
     }
 };
 
@@ -55,7 +55,7 @@ void solve(){
         trie.insert(s);
     }
     int q;cin >> q;
-    while(q--){
+    while(q --){
         string x;cin >> x;
         if(trie.find(x))cout << "Exist" << '\n';
         else cout << "Nope" << '\n';
