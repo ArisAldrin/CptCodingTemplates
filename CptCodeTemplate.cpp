13de1927e6e3bc9efbb7e5ae628f6ac0fb85601e
ArisAldrin <1469207005@qq.com>
2025-12-17 08:48:24 +0800

    bug: ./rall and RALL

--- CptCodeTemplate.cpp
+++ CptCodeTemplate.cpp
@@ -9,40 +9,38 @@
 #define NOO cout << -1 << '\n';
 #define all(x) (x).begin() , (x).end()
 #define ALL(X) (X).begin() + 1 , (X).end()
-#define rall(x) (x).rbegin() , (x).end()
-#define RALL(X) (X).rbegin() , (X).end() - 1
+#define rall(x) (x).rbegin() , (x).rend()
+#define RALL(X) (X).rbegin() , (X).rend() - 1
 #define fs first
 #define sc second
 #define arr array
 #define int long long
 
 using namespace std;
-using ll = long long;
 using ull = unsigned long long;
 using db = double;
 using pii = pair<int , int>;
-using pll = pair<ll , ll>;
 using pdd = pair<db , db>;
 using i128 = __int128_t;
 
 const int inf = 1e9 + 7;
 const int nil = -inf;
-const ll INF = 1e18;
-const ll NIL = -INF;
-const ll MOD = 998244353;
+const int INF = 1e18;
+const int NIL = -INF;
+const int MOD = 998244353;
 const db EPS = 1e-10;
 const db PI = acos(-1);
 mt19937 rdint(time(0));
 mt19937_64 rdll(time(0));
 
 void solve(){
-    
+
 }
 
 signed main(){
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    cout << fixed << setprecision(10);
+    cout << fixed << setprecision(15);
     int t;cin >> t;
     while(t --)
     solve();
